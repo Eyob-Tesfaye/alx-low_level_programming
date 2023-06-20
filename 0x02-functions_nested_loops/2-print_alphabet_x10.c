c3fefd9a7efd73eb99a00e79c22b91ead9dd5692
Eyob-Tesfaye <eyoteshab@gmail.com>
2023-06-20 14:07:40 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+
+
+void print_alphabet_x10(void)
+{
+	int i = 1;
+	char c = 'a';
+	
+	do
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++
+		
+		}
+	
+	
+	}
+	while (i <= 10 )
+
+}
