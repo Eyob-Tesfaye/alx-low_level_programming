a652edf33259f3a38b56cdc96afb5d97cff49754
Eyob-Tesfaye <eyoteshab@gmail.com>
2023-06-26 11:26:05 -0700

    G

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/* swap_int - swaps two int values
+ * @a: int
+ * @b: Int
+ *
+ * Return: nothing.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
