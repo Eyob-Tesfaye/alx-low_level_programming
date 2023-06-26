aa1d2f0edd99f444c6783bda74a93fe0819c71b7
Eyob-Tesfaye <eyoteshab@gmail.com>
2023-06-26 11:05:54 -0700

    fs

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * modif_my_param - this function does not modify n
- * @*n: 98 
+ * reset_to_98 - this function does not modify n
+ * @n: 98 
  *
  * Return: nothing.
  */
