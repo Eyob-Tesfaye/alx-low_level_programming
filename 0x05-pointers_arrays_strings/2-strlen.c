0e0d9de89d1871187cffd21152ad4fc8f09a235c
Eyob-Tesfaye <eyoteshab@gmail.com>
2023-06-26 13:15:27 -0700

    NN

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,13 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _strlen - 
- * @s: int
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: nothing.
+ * Return: the length of the string
  */
 int _strlen(char *s)
-{	
+{
 	int i;
 
 	i = 0;
